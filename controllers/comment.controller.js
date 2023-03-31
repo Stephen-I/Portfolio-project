70f8f7ef69b4e127660d1b33a1eaeda613235378
Stephen Irinoye <stephenirinoye@outlook.com>
2023-03-31 15:18:22 +0100

    ticket 7 refactored

--- controllers/comment.controller.js
+++ controllers/comment.controller.js
@@ -1,4 +1,5 @@
 const { seeCommentsById, addComment } = require("../models/comment.model");
+const { seeReviewsById } = require("../models/review.model");
 
 exports.viewComments = (req, res, next) => {
   const { review_id } = req.params;
@@ -9,8 +10,13 @@ exports.viewComments = (req, res, next) => {
 
 exports.insertComments = (req, res, next) => {
   const { review_id } = req.params;
-  const { author, body } = req.body;
-  addComment(author, body, review_id).then((comment) => {
-    res.status(201).send({ comment });
-  });
+  const { username: author, body } = req.body;
+  seeReviewsById(review_id)
+    .then(() => {
+      return addComment(author, body, review_id);
+    })
+    .then((comment) => {
+      res.status(201).send({ comment });
+    })
+    .catch(next);
 };
