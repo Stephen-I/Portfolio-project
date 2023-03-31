9341e953b1bb8570a84a65813efd1f53eb759bd9
Stephen Irinoye <stephenirinoye@outlook.com>
2023-03-31 14:19:35 +0100

    setting up host

--- listen.js
+++ listen.js
@@ -1,8 +1,4 @@
 const app = require("./app");
+const { PORT = 9090 } = process.env;
 
-app.listen(9090, (err) => {
-  if (err) {
-    console.log(err);
-  }
-  console.log("server is listening on port 9090");
-});
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
