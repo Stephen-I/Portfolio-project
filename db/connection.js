b19495032266e76eb88e6ee235d96dffb18016c2
Stephen Irinoye <stephenirinoye@outlook.com>
2023-04-13 17:27:05 +0100

    fixed

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,10 @@
 const { Pool } = require("pg");
 const ENV = process.env.NODE_ENV || "development";
+
+require("dotenv").config({
+  path: `${__dirname}/../.env.${ENV}`,
+});
+
 const config =
   ENV === "production"
     ? {
@@ -8,12 +13,8 @@ const config =
       }
     : {};
 
-require("dotenv").config({
-  path: `${__dirname}/../.env.${ENV}`,
-});
-
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
-module.exports = new Pool();
+module.exports = new Pool(config);
